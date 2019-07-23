944a848a167c16aa51b734a3565f46505fa5f864
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2019-07-23 20:36:18 +0200

    refresh packages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const Telegraf          = require('telegraf');
-const bot               = require('./modules/bot');
-const {token}           = require('./config/keys');
-const axios             = require("axios");
+const Telegraf = require('telegraf');
+const bot = require('./modules/bot');
+const { token } = require('./config/keys');
+const axios = require("axios");
 // Instance of Telegraf
 const chat = new Telegraf(token);
 // Initialize bot
@@ -12,41 +12,42 @@ bot.init()
 chat.start((ctx) => ctx.reply('Hi mate, tell me how can I help you!'))
 chat.help((ctx) => ctx.reply('Ask me something'));
 chat.on('sticker', (ctx) => ctx.reply('👍'))
-    
-chat.hears('Who is the real king?', (ctx) => ctx.reply(`No, the REAL KING is ${ctx.from.first_name+" "+ctx.from.last_name}, my dear lord.`));
+
+chat.hears('Who is the real king?', (ctx) => ctx.reply(`No, the REAL KING is ${ctx.from.first_name + " " + ctx.from.last_name}, my dear lord.`));
 
 // The chat is listening for message
 chat.hears(/planet (.+)/i, (ctx) => {
     let user = ctx.from.first_name;
     let query = ctx.message.text.trim();
-    try{
+    try {
         // Get only the info about the planet choosen
-        query=query.split("").slice(("planet ").split("").length).join("");
-    } catch (err){
+        query = query.split("").slice(("planet ").split("").length).join("");
+    } catch (err) {
         console.log(err)
     }
     console.log(query)
     axios.get(`https://swapi.co/api/planets/${query}`)
-        .then(res=>{
+        .then(res => {
             // console.log(res.data)
-            if(res.data.population){
+            if (res.data.population) {
                 ctx.reply(`${user}, the planet ${res.data.name} of Star Wars that you've asked for has ${res.data.population} inhabitants.`);
             } else {
                 Promise.reject()
             }
         })
-        .catch (err=>{
+        .catch(err => {
             console.log(err)
-            ctx.reply(`I haven't found this planet, type something like: "planet" 5`)} 
-            )
-    })
+            ctx.reply(`I haven't found this planet, type something like: "planet" 5`)
+        }
+        )
+})
 
 
-    // On non standard message responds the rivescript bot
+// On non standard message responds the rivescript bot
 chat.on('message', (ctx) => {
     // let user = ctx.from.first_name;
     let message = ctx.message.text.trim();
-    bot.ask(message,(reply)=>{
+    bot.ask(message, (reply) => {
         return ctx.reply(reply);
     })
 })
